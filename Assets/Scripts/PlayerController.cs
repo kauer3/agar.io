03066ec8900f666a75c6b81b4447e6f973dfc04e
Kauê Reis <kaue0607@gmail.com>
2023-07-28 17:03:20 +1200

    Implement destroy second player and reset camera

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,20 +7,25 @@ public class PlayerController : MonoBehaviour
     public float mass = 1;
     public float speed = 0.2f;
     Vector3 velocity;
+    public bool player1;
     new Camera camera;
 
     void Update()
     {
         transform.localScale = new Vector3(mass, mass, mass);
-        Vector3 input = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        Vector3 input = player1 ? new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0) : new Vector3(Input.GetAxis("P2Horizontal"), Input.GetAxis("P2Vertical"), 0);
         Vector3 direction = input.normalized;
         velocity = direction * speed;
+    }
+
+    void Start()
+    {
+        camera = transform.GetComponentInChildren<Camera>();
     }
 
     private void FixedUpdate()
     {
         transform.Translate(velocity);
-
     }
 
     private void OnTriggerEnter(Collider triggerCollider)
@@ -29,9 +34,23 @@ public class PlayerController : MonoBehaviour
         {
             Pickup pickup = triggerCollider.GetComponentInParent<Pickup>();
             mass += pickup.mass;
-            Destroy(triggerCollider.gameObject);
-            // calculate camera size relative to player size
-            Camera.main.orthographicSize = mass * 2;
+            DestroyGameObject(triggerCollider.gameObject);
         }
+        else
+        {
+            PlayerController player = triggerCollider.GetComponentInParent<PlayerController>();
+            if (mass > player.mass)
+            {
+                mass += player.mass;
+                DestroyGameObject(triggerCollider.gameObject);
+                camera.rect = new Rect(0, 0, 1, 1);
+            }
+        }
+    }
+
+    private void DestroyGameObject(GameObject objectToBedestroyed)
+    {
+            Destroy(objectToBedestroyed);
+            camera.orthographicSize = mass * 2;
     }
 }
